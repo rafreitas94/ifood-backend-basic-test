c52a3c78c34ad79b48334bbcada9e210038af623
Raphael <raphaelsfreitas94@gmail.com>
2021-12-13 20:17:38 -0300

    task 3

--- src/main/java/com/ifood/controllers/WeatherController.java
+++ src/main/java/com/ifood/controllers/WeatherController.java
@@ -4,6 +4,7 @@ import com.ifood.client.OpenWheaterClient;
 import com.ifood.model.WeatherClient;
 import com.ifood.model.response.WeatherClientResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ public class WeatherController {
     }
 
     @GetMapping
+    @Cacheable("city")
     public ResponseEntity<WeatherClientResponse> getWeather(@RequestParam("city") String city) {
         WeatherClient weather = openWheaterClient.getWeather(city, weatherAppId);
         return ResponseEntity.status(weather.getCod()).body(new WeatherClientResponse(weather));
